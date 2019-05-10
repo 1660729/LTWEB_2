082bd03030710e6c20cbea38edfa4ee766e3dc92
VinhLe <43854764+1660729@users.noreply.github.com>
2019-05-10 19:13:21 +0700

    setup trang web

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var morgan = require('morgan');
+var exphbs = require('express-handlebars');
+var createError = require('http-errors');
+
+var app = express();
+
+app.use(morgan('dev'));
+
+// su dung express-handlebars 
+app.engine('hbs', exphbs({
+    defaultLayout: 'main.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// su dung phuong thuc post
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+// render toi trang can su dung
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/error', (req, res) => {
+    res.render('error', { layout: false });
+});
+
+app.use((req, res, next) => {
+    next(createError(404));
+})
+
+app.use((err, req, res, next) => {
+    var status = err.status || 500;
+    var vwErr = 'error';
+
+    if (status === 404) {
+        vwErr = '404';
+    }
+
+    var message = err.message;
+    var error = err;
+
+    res.status(status).render(vwErr, {
+        layout: false,
+        message,
+        error
+    });
+
+})
+
+var port = 3000;
+app.listen(port, () => {
+    console.log(`server is running at port ${port}`);
+});
