58794a11d4451e1e96df98f56945b7a0547ee5bc
VinhLe <43854764+1660729@users.noreply.github.com>
2019-05-10 15:29:32 +0700

    Create baodientu.sql
    database

---
+++ database/baodientu.sql
@@ -0,0 +1,81 @@
+/*
+ Date: 10/05/2019 15:11:08
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for baiviet
+-- ----------------------------
+DROP TABLE IF EXISTS `baiviet`;
+CREATE TABLE `baiviet`  (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TieuDe` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `ChuyenMuc` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `NgayDang` datetime(6) NULL DEFAULT NULL,
+  `AnhDaiDien` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `NoiDungTomTat` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `Youtube` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `LuotXem` int(255) NULL DEFAULT NULL,
+  `TinhTrang` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for chitietbaiviet
+-- ----------------------------
+DROP TABLE IF EXISTS `chitietbaiviet`;
+CREATE TABLE `chitietbaiviet`  (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `AnhDaiDien` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `TieuDe` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `Youtube` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `ChuyenMuc` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `Nhan` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `NgayDang` datetime(6) NULL DEFAULT NULL,
+  `NoiDung` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `TinhTrang` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for chuyenmuc
+-- ----------------------------
+DROP TABLE IF EXISTS `chuyenmuc`;
+CREATE TABLE `chuyenmuc`  (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TenChuyenMuc` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for docgia
+-- ----------------------------
+DROP TABLE IF EXISTS `docgia`;
+CREATE TABLE `docgia`  (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TenDocGia` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `NgayBinhLuan` datetime(6) NULL DEFAULT NULL,
+  `NoiDungBL` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `ThoiHan` date NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for taikhoan
+-- ----------------------------
+DROP TABLE IF EXISTS `taikhoan`;
+CREATE TABLE `taikhoan`  (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NguoiDung` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `MatKhau` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `HoTen` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `ButDanh` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `Email` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `NgaySinh` date NULL DEFAULT NULL,
+  `PhanQuyen` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
