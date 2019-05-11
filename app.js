21fb7ff3b004bfc817c196a3230001b93a4211d4
VinhLe <43854764+1660729@users.noreply.github.com>
2019-05-11 22:30:32 +0700

    template trang home

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var app = express();
 
 app.use(morgan('dev'));
 
+app.use(express.static('public'));
+
 // su dung express-handlebars 
 app.engine('hbs', exphbs({
     defaultLayout: 'main.hbs'
@@ -26,32 +28,32 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/error', (req, res) => {
-    res.render('error', { layout: false });
-});
+// app.get('/error', (req, res) => {
+//     res.render('error', { layout: false });
+// });
 
-app.use((req, res, next) => {
-    next(createError(404));
-})
+// app.use((req, res, next) => {
+//     next(createError(404));
+// })
 
-app.use((err, req, res, next) => {
-    var status = err.status || 500;
-    var vwErr = 'error';
+// app.use((err, req, res, next) => {
+//     var status = err.status || 500;
+//     var vwErr = 'error';
 
-    if (status === 404) {
-        vwErr = '404';
-    }
+//     if (status === 404) {
+//         vwErr = '404';
+//     }
 
-    var message = err.message;
-    var error = err;
+//     var message = err.message;
+//     var error = err;
 
-    res.status(status).render(vwErr, {
-        layout: false,
-        message,
-        error
-    });
+//     res.status(status).render(vwErr, {
+//         layout: false,
+//         message,
+//         error
+//     });
 
-})
+// })
 
 var port = 3000;
 app.listen(port, () => {
